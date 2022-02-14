7789bc262bdc50b2acc5833114bb20e108b60bde
konte <konte@DESKTOP-N7VSOVP>
2022-02-14 20:04:08 +0100

    Ajoutez des fichiers projet.

---
+++ AppStock/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace AppStock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgProduit = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLibelle = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtQte = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPU = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnModifier = new System.Windows.Forms.Button();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            this.btnQuitter = new System.Windows.Forms.Button();
+            this.btnSelectionner = new System.Windows.Forms.Button();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnRechercher = new System.Windows.Forms.Button();
+            this.txtRQteStockSup = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtRQteStockInf = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtRDescription = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtRLibelle = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgProduit)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgProduit
+            // 
+            this.dgProduit.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgProduit.Location = new System.Drawing.Point(356, 163);
+            this.dgProduit.Name = "dgProduit";
+            this.dgProduit.Size = new System.Drawing.Size(745, 433);
+            this.dgProduit.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Libelle";
+            // 
+            // txtLibelle
+            // 
+            this.txtLibelle.Location = new System.Drawing.Point(25, 95);
+            this.txtLibelle.Name = "txtLibelle";
+            this.txtLibelle.Size = new System.Drawing.Size(289, 20);
+            this.txtLibelle.TabIndex = 2;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(25, 192);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(289, 20);
+            this.txtDescription.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Description";
+            // 
+            // txtQte
+            // 
+            this.txtQte.Location = new System.Drawing.Point(25, 289);
+            this.txtQte.Name = "txtQte";
+            this.txtQte.Size = new System.Drawing.Size(289, 20);
+            this.txtQte.TabIndex = 6;
+            this.txtQte.TextChanged += new System.EventHandler(this.txtQte_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 260);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Quantité en stock";
+            // 
+            // txtPU
+            // 
+            this.txtPU.Location = new System.Drawing.Point(25, 386);
+            this.txtPU.Name = "txtPU";
+            this.txtPU.Size = new System.Drawing.Size(289, 20);
+            this.txtPU.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 357);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Prix unitaire";
+            // 
+            // btnModifier
+            // 
+            this.btnModifier.Location = new System.Drawing.Point(136, 452);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(75, 23);
+            this.btnModifier.TabIndex = 10;
+            this.btnModifier.Text = "&Modifier";
+            this.btnModifier.UseVisualStyleBackColor = true;
+            this.btnModifier.Click += new System.EventHandler(this.btnModifier_Click);
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.Location = new System.Drawing.Point(247, 452);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(75, 23);
+            this.btnSupprimer.TabIndex = 11;
+            this.btnSupprimer.Text = "&Supprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = true;
+            this.btnSupprimer.Click += new System.EventHandler(this.btnSupprimer_Click);
+            // 
+            // btnQuitter
+            // 
+            this.btnQuitter.Location = new System.Drawing.Point(25, 573);
+            this.btnQuitter.Name = "btnQuitter";
+            this.btnQuitter.Size = new System.Drawing.Size(75, 23);
+            this.btnQuitter.TabIndex = 12;
+            this.btnQuitter.Text = "&Quitter";
+            this.btnQuitter.UseVisualStyleBackColor = true;
+            this.btnQuitter.Click += new System.EventHandler(this.btnQuitter_Click);
+            // 
+            // btnSelectionner
+            // 
+            this.btnSelectionner.Location = new System.Drawing.Point(248, 22);
+            this.btnSelectionner.Name = "btnSelectionner";
+            this.btnSelectionner.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectionner.TabIndex = 13;
+            this.btnSelectionner.Text = "S&electionner";
+            this.btnSelectionner.UseVisualStyleBackColor = true;
+            this.btnSelectionner.Click += new System.EventHandler(this.btnSelectionner_Click);
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Location = new System.Drawing.Point(25, 452);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(75, 23);
+            this.btnAjouter.TabIndex = 14;
+            this.btnAjouter.Text = "&Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            this.btnAjouter.Click += new System.EventHandler(this.btnAjouter_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.btnRechercher);
+            this.panel1.Controls.Add(this.txtRQteStockSup);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.txtRQteStockInf);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.txtRDescription);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.txtRLibelle);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.ForeColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(356, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(745, 132);
+            this.panel1.TabIndex = 15;
+            // 
+            // btnRechercher
+            // 
+            this.btnRechercher.BackColor = System.Drawing.Color.White;
+            this.btnRechercher.ForeColor = System.Drawing.Color.Gray;
+            this.btnRechercher.Location = new System.Drawing.Point(656, 93);
+            this.btnRechercher.Name = "btnRechercher";
+            this.btnRechercher.Size = new System.Drawing.Size(75, 23);
+            this.btnRechercher.TabIndex = 16;
+            this.btnRechercher.Text = "&Rechercher";
+            this.btnRechercher.UseVisualStyleBackColor = false;
+            this.btnRechercher.Click += new System.EventHandler(this.btnRechercher_Click);
+            // 
+            // txtRQteStockSup
+            // 
+            this.txtRQteStockSup.Location = new System.Drawing.Point(568, 51);
+            this.txtRQteStockSup.Name = "txtRQteStockSup";
+            this.txtRQteStockSup.Size = new System.Drawing.Size(163, 20);
+            this.txtRQteStockSup.TabIndex = 19;
+            this.txtRQteStockSup.TextChanged += new System.EventHandler(this.textQteStockSup_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(565, 20);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(152, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Quantité en stock supérieur à  ";
+            // 
+            // txtRQteStockInf
+            // 
+            this.txtRQteStockInf.Location = new System.Drawing.Point(384, 51);
+            this.txtRQteStockInf.Name = "txtRQteStockInf";
+            this.txtRQteStockInf.Size = new System.Drawing.Size(163, 20);
+            this.txtRQteStockInf.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(381, 20);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(140, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Quantité en stock inférieur à";
+            // 
+            // txtRDescription
+            // 
+            this.txtRDescription.Location = new System.Drawing.Point(200, 51);
+            this.txtRDescription.Name = "txtRDescription";
+            this.txtRDescription.Size = new System.Drawing.Size(163, 20);
+            this.txtRDescription.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(197, 20);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Description";
+            // 
+            // txtRLibelle
+            // 
+            this.txtRLibelle.Location = new System.Drawing.Point(16, 51);
+            this.txtRLibelle.Name = "txtRLibelle";
+            this.txtRLibelle.Size = new System.Drawing.Size(163, 20);
+            this.txtRLibelle.TabIndex = 17;
+            this.txtRLibelle.TextChanged += new System.EventHandler(this.textRLibelle_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Libelle";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1113, 608);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnAjouter);
+            this.Controls.Add(this.btnSelectionner);
+            this.Controls.Add(this.btnQuitter);
+            this.Controls.Add(this.btnSupprimer);
+            this.Controls.Add(this.btnModifier);
+            this.Controls.Add(this.txtPU);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtQte);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLibelle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgProduit);
+            this.Name = "Form1";
+            this.Text = "Formulaire Produit";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgProduit)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgProduit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLibelle;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtQte;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPU;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnModifier;
+        private System.Windows.Forms.Button btnSupprimer;
+        private System.Windows.Forms.Button btnQuitter;
+        private System.Windows.Forms.Button btnSelectionner;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnRechercher;
+        private System.Windows.Forms.TextBox txtRQteStockSup;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtRQteStockInf;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtRDescription;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtRLibelle;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ AppStock/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AppStock.Model;
+
+namespace AppStock
+{
+    public partial class Form1 : Form
+    {  
+        bdStockEntities db = new bdStockEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnQuitter_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgProduit.DataSource = db.Produit.ToList();
+        }
+
+        private void Effacer()
+        {
+            txtDescription.Text = "";
+            txtLibelle.Text = "";
+            txtPU.Text = "";
+            txtQte.Text = "";
+            dgProduit.DataSource = db.Produit.ToList();
+            txtLibelle.Focus();
+        }
+
+        
+
+        private void btnAjouter_Click(object sender, EventArgs e)
+        {
+            Produit p = new Produit();
+            p.LibelleProduit = txtLibelle.Text;
+            p.DescriptionProduit = txtDescription.Text;
+            p.PUProduit = double.Parse(txtPU.Text);
+            p.QteProduit = double.Parse(txtQte.Text);
+            db.Produit.Add(p);
+            db.SaveChanges();
+
+            Effacer();
+
+        }
+
+        private void btnModifier_Click(object sender, EventArgs e)
+        {
+            int? id = int.Parse(dgProduit.CurrentRow.Cells[0].Value.ToString());
+            Produit p = db.Produit.Find(id);
+            p.LibelleProduit = txtLibelle.Text;
+            p.DescriptionProduit = txtDescription.Text;
+            p.PUProduit = double.Parse(txtPU.Text);
+            p.QteProduit = double.Parse(txtQte.Text);
+            
+            db.SaveChanges();
+            Effacer();
+        }
+
+        private void txtQte_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSupprimer_Click(object sender, EventArgs e)
+        {
+            int? id = int.Parse(dgProduit.CurrentRow.Cells[0].Value.ToString());
+            Produit p = db.Produit.Find(id);
+            db.Produit.Remove(p);
+
+            db.SaveChanges();
+            Effacer();
+        }
+
+        private void btnSelectionner_Click(object sender, EventArgs e)
+        {
+            txtDescription.Text = dgProduit.CurrentRow.Cells[2].Value.ToString();
+            txtLibelle.Text = dgProduit.CurrentRow.Cells[1].Value.ToString();
+            txtPU.Text = dgProduit.CurrentRow.Cells[4].Value.ToString();
+            txtQte.Text = dgProduit.CurrentRow.Cells[3].Value.ToString();
+            
+        }
+
+        private void btnRechercher_Click(object sender, EventArgs e)
+        {
+            var liste = db.Produit.ToList();
+            if(!string.IsNullOrEmpty(txtRLibelle.Text))
+            {
+                liste = liste.Where(a => a.LibelleProduit.ToUpper().Contains(txtRLibelle.Text.ToUpper())).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(txtRDescription.Text))
+            {
+                liste = liste.Where(a => a.DescriptionProduit.ToUpper().Contains(txtRDescription.Text.ToUpper())).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(txtRQteStockInf.Text))
+            {
+                double qte = double.Parse(txtRQteStockInf.Text);
+                liste = liste.Where(a => a.QteProduit <= qte).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(txtRQteStockSup.Text))
+            {
+                double qte = double.Parse(txtRQteStockSup.Text);
+                liste = liste.Where(a => a.QteProduit >= qte).ToList();
+            }
+            dgProduit.DataSource = liste;
+        }
+
+        private void textRLibelle_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textQteStockSup_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
